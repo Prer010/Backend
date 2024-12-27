497d6083051a2495610f589527a47d9fff99e978
Prer010 <prerp.6299@gmail.com>
2024-12-27 16:21:38 +0530

    Add new files

---
+++ 4.2 Passing Data/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let html = "Enter your name here : ";
+app.get("/", (req, res) => {
+  res.render("index.ejs" ,{ html });
+});
+
+app.post("/submit", (req, res) => {
+var letters = req.body["fName"].length + req.body["lName"].length;
+html = `Your have ${letters} letters in your name`; 
+res.render("index.ejs" ,{ html })
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
